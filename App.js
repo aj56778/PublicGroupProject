222366f0d5b488b6ce42296b8ab66da39abc8eb6
aj56778 <108079946+aj56778@users.noreply.github.com>
2023-04-25 10:09:46 -0400

    Update App.js

--- App.js
+++ App.js
@@ -4,19 +4,22 @@ import { useState } from 'react';
 import CoverPage from './CoverPage';
 import Component from './component.js';
 import image from "./image-placeholder.jpg";
+import Register from './Register';
+import { useNavigate } from 'react-router-dom';
 function App() {
+  const navigate = useNavigate();
   const isLoggedIn = false;
   return (
     <div className="App">
       <header className="App-header"></header>
       <div class="logo">VICI</div>
       <div>
-      {isLoggedIn ? (
+      {/* {isLoggedIn ? (
         <button class="bttn specialButtons" id="logout" onClick={() => isLoggedIn=false}>Logout</button>
-      ) : (
-        <><button class="bttn specialButtons" id="signup">Sign Up</button>
-        <button class="bttn specialButtons" id="login" onClick={() => {isLoggedIn=true}}>Login</button></>
-      )}
+      ) : ( */}
+        <button class="bttn specialButtons" id="signup" onClick={navigate('/Register')}>Sign Up</button>
+        <button class="bttn specialButtons" id="login" onClick={() => {isLoggedIn=true}}>Login</button>
+      {/* )} */}
       </div>
       
       <div className="arrow"></div>
